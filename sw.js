a4c8951498e71b8ed6a3adc091e6fb4371062bb4
Go1den <gooneden@gmail.com>
2026-05-05 00:11:04 -0500

    puzgod

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CRITICAL: You MUST change this string (e.g., v3, v4) every time you deploy a new update!
-const CACHE_NAME = "xw-solver-v2026.5.2";
+const CACHE_NAME = "puzgod-v2026.5.5";
 
 const ASSETS = [
     "./",
